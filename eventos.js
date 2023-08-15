04123881897efdf7652fe15f075b33fd8b2ef2a5
emiliapalleiro <epalleiro@gmail.com>
2023-08-15 11:27:10 -0300

    evento

--- eventos.js
+++ eventos.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let contenedorBoton = document.querySelector("div");
 
     contenedorBoton.addEventListener("click", function(event) {
-        if (event.target === contenedorBoton) {
+        if (event.target === contenedorBoton) { //event.target lo utilizamos para verificar si el click se hizo en el contenedor y no en el boton dentro del contenedor
             alert("Hola! Soy el Div");
         }
     })
